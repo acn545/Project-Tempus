94fb0bf1991e59ce850a04a630e3ade937de2a31
Anthony <anthonycnoble@live.com>
2018-08-02 15:24:54 -0700

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bteam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
